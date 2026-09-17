652f3e8ea2a0223689deab3b12f21f39b855c4f9
singhadiya <40931572+singhadiya@users.noreply.github.com>
2026-09-17 12:33:06 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[]ans = new int[nums.length];
+        Arrays.fill(ans,1);
+        for(int i=1;i<nums.length; i++){
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+        int rightMultiplier = 1;
+        for(int i=nums.length-1;i>=0; i--){
+            ans[i] = ans[i]*rightMultiplier;
+            rightMultiplier = rightMultiplier*nums[i];
+        }
+        return ans;
+    }
+}  
