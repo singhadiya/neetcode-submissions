ac5335f9ac6060effb04acafcd58721cbeb9e95f
singhadiya <40931572+singhadiya@users.noreply.github.com>
2026-09-19 17:52:04 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int ans = 0;
+        Map<Character,Integer>freq = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        while(right<s.length()){
+            char ch = s.charAt(right);
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+            maxFreq = Math.max(maxFreq,freq.get(ch));
+            if(right-left+1-k<=maxFreq){
+                ans = Math.max(ans,right-left+1);
+            }
+            if(right-left+1-k>maxFreq){
+                freq.put(s.charAt(left),freq.get(s.charAt(left))-1);
+                left++;
+            }
+            // System.out.println("freq : "+freq + " , right : "+right+ " , left : "+left+ " , ans : "+ans+" , maxFreq : "+maxFreq);
+            right++;
+        }
+        return ans;
+
+
+        // Set<Character>ch = new HashSet<>();
+        // for(char c : s.toCharArray()){
+        //     ch.add(c);
+        // }
+        // int ans =0;
+        // for(char c :ch){
+        //     int freq = 0;
+
+        //     int left=0;
+        //     int right=0;
+        //     while(right<s.length()){
+        //         if(s.charAt(right)==c){
+        //             freq++;
+        //         }
+        //         if(right-left+1-k<=freq){
+        //             ans = Math.max(ans,right-left+1);
+        //         }
+        //         while(right-left+1-k>freq){
+        //             if(s.charAt(left)==c){
+        //                 freq--;
+        //             }
+        //             left++;
+        //         }
+        //         right++;
+        //     }
+        // }
+        // return ans;
+    }
+}
