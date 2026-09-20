a5c4f976cabdd4b8d916d4889f434c21eb43617b
singhadiya <40931572+singhadiya@users.noreply.github.com>
2026-09-20 15:02:37 +0530

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        String ans = "";
+        int sLen = s.length();
+        int length = Integer.MAX_VALUE;
+        int tLen = t.length();
+        if(sLen<tLen) return ans;
+        int[]sArray = new int[64];
+        int[]tArray = new int[64];
+        int diff = tLen;
+        for(int i=0;i<tLen;i++){
+            tArray[t.charAt(i)-'A']++;
+        }
+        // print(tArray);
+        int left=0;
+        int right=0;
+        while(right<sLen){
+            char ch = s.charAt(right);
+            if(tArray[ch-'A']>0){
+                if(sArray[ch-'A']<tArray[ch-'A']){
+                     diff--;
+                }
+            sArray[ch-'A']++;
+            }
+                while(left<=right && diff==0){
+                    if(right-left+1<length){
+                    ans = s.substring(left,right+1);
+                    length = right-left+1;
+                    }
+                    int index = s.charAt(left)-'A';
+                    if(sArray[index]>0){
+                        if(sArray[index]<=tArray[index]){
+                            diff++;
+                        }
+                        sArray[index]--;
+                    }
+                    left++;
+                }
+            // System.out.print("ans : "+ans+" , left : "+left+" , right : "+right+" ::: ");
+            // print(sArray);
+            right++;
+        }
+        return ans;
+    }
+    public void print(int[]temp){
+        for(int i=0;i<26;i++){
+            System.out.print(temp[i]+" ");
+        }
+        System.out.println();
+    }
+}
